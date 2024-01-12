170d52f567a6e3ecbc917cff8a849092f89af99c
elciddelapaz <127919521+RomwelFeliciano@users.noreply.github.com>
2024-01-12 15:48:28 +0800

    Refactor: Promo and PromoItem

--- src/components/promo-item.js
+++ src/components/promo-item.js
@@ -1,86 +1,34 @@
-export default function PromoItem({ promos, addToCart, cartItem }) {
+export default function PromoItem({ promo, addToCart, cartItem }) {
+  // Get all the types of Item in the Data
+  const itemType = [
+    ...new Set(promo.items.map((itemType) => itemType.itemType)),
+  ];
+
   return (
     <>
-      {promos.map((promo) => (
-        <div
-          className="col-span-12 flex flex-col gap-4 p-4 bg-neutral-900 rounded"
-          key={promo.promoId}
-        >
-          <h1 className="text-center font-semibold bg-neutral-950 rounded-lg py-2">
-            Promo {promo.promoId} --- Lists
-          </h1>
-
-          <div className="flex items-center gap-3 pt-3">
-            <h2 className="w-24">Fruits:</h2>
-            {promo.items
-              .filter((item) => item.itemType === "fruit")
-              .map((item) => (
-                <button
-                  className={`w-64 py-2 rounded-lg cursor-pointer capitalize ${
-                    cartItem.some(
-                      (cItem) =>
-                        cItem.item === item.item &&
-                        cItem.inCart &&
-                        cItem.promoId === promo.promoId
-                    )
-                      ? "bg-red-700"
-                      : "bg-sky-600 hover:bg-sky-800"
-                  }`}
-                  key={item.item}
-                  onClick={() => addToCart(item, promo.promoId)}
-                >
-                  {item.item}
-                </button>
-              ))}
-          </div>
-
-          <div className="flex items-center gap-3 pt-3">
-            <h2 className="w-24">Proteins:</h2>
-            {promo.items
-              .filter((item) => item.itemType === "protein")
-              .map((item) => (
-                <button
-                  className={`w-64 py-2 rounded-lg cursor-pointer capitalize ${
-                    cartItem.some(
-                      (cItem) =>
-                        cItem.item === item.item &&
-                        cItem.inCart &&
-                        cItem.promoId === promo.promoId
-                    )
-                      ? "bg-red-700"
-                      : "bg-sky-600 hover:bg-sky-800"
-                  }`}
-                  key={item.item}
-                  onClick={() => addToCart(item, promo.promoId)}
-                >
-                  {item.item}
-                </button>
-              ))}
-          </div>
-
-          <div className="flex items-center gap-3 pt-3">
-            <h2 className="w-24">Carbs:</h2>
-            {promo.items
-              .filter((item) => item.itemType === "carb")
-              .map((item) => (
-                <button
-                  className={`w-64 py-2 rounded-lg cursor-pointer capitalize ${
-                    cartItem.some(
-                      (cItem) =>
-                        cItem.item === item.item &&
-                        cItem.inCart &&
-                        cItem.promoId === promo.promoId
-                    )
-                      ? "bg-red-700"
-                      : "bg-sky-600 hover:bg-sky-800"
-                  }`}
-                  key={item.item}
-                  onClick={() => addToCart(item, promo.promoId)}
-                >
-                  {item.item}
-                </button>
-              ))}
-          </div>
+      {itemType.map((iType) => (
+        <div className="flex items-center gap-3 pt-3">
+          <h2 className="w-24 capitalize">{iType}s:</h2>
+          {promo.items
+            .filter((item) => item.itemType === iType)
+            .map((item) => (
+              <button
+                className={`w-64 py-2 rounded-lg cursor-pointer capitalize ${
+                  cartItem.some(
+                    (cItem) =>
+                      cItem.item === item.item &&
+                      cItem.inCart &&
+                      cItem.promoId === promo.promoId
+                  )
+                    ? "bg-red-700"
+                    : "bg-sky-600 hover:bg-sky-800"
+                }`}
+                key={item.item}
+                onClick={() => addToCart(item, promo.promoId)}
+              >
+                {item.item}
+              </button>
+            ))}
         </div>
       ))}
     </>
--- src/components/promo.js
+++ src/components/promo.js
@@ -4,7 +4,17 @@ export default function Promo({ promos, addToCart, cartItem }) {
   return (
     <div className="col-span-12 flex flex-col justify-start items-center gap-5">
       <h1 className="text-3xl font-bold">Menu of each Promo</h1>
-      <PromoItem promos={promos} addToCart={addToCart} cartItem={cartItem} />
+      {promos.map((promo) => (
+        <div
+          className="col-span-12 flex flex-col gap-4 p-4 bg-neutral-900 rounded"
+          key={promo.promoId}
+        >
+          <h1 className="text-center font-semibold bg-neutral-950 rounded-lg py-2">
+            Promo {promo.promoId} --- Lists
+          </h1>
+          <PromoItem promo={promo} addToCart={addToCart} cartItem={cartItem} />
+        </div>
+      ))}
     </div>
   );
 }
